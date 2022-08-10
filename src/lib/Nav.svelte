edfadbc6b6d8188a548cc75e7dda2797e69252b7
Oliver Braun <ob@cs.hm.edu>
2022-08-10 11:20:09 +0200

    change semester via dropdown

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -2,8 +2,8 @@
 	import { request, gql } from 'graphql-request';
 	import { semester, allSemesterNames } from '../stores/semester.js';
 	import { fetchZPA } from '../stores/zpa.js';
-	import { isOverlayOpen } from '../stores/overlay';
-	import Overlay from '$lib/Overlay.svelte';
+
+	let dropdownOpen = false;
 
 	function setSemester(sem) {
 		const mutation = gql`
@@ -15,29 +15,11 @@
         `;
 
 		request('http://localhost:8080/query', mutation).then((data) => {
-			console.log(`set semester to ${data}`);
 			semester.set(data.setSemester.id);
-			fetchZPA();
-			isOverlayOpen.set(false);
+			dropdownOpen = false;
 		});
 	}
 </script>
-
-{#if $isOverlayOpen}
-	<Overlay>
-		<div class="text-center">
-			<h1>Semester auswählen:</h1>
-			{#each $allSemesterNames as semesterName}
-				<button
-					class="bg-green-200 text-black font-bold py-2 px-4 m-4 rounded hover:bg-green-500"
-					on:click={setSemester(semesterName)}
-				>
-					{semesterName}
-				</button>
-			{/each}
-		</div>
-	</Overlay>
-{/if}
 
 <nav class="flex justify-center w-full">
 	<a class="mx-4 text-lg" href="/">Plexams</a>
@@ -45,10 +27,42 @@
 	<a class="mx-4 text-lg" href="/zpa/invigilators">Aufsichten</a>
 	<a class="mx-4 text-lg" href="/zpa/exams">Prüfungsliste (ZPA)</a>
 	<a class="mx-4 text-lg" href="/primuss">Prüfungsliste (Primuss)</a>
-	<button
-		class="mr-4 text-lg bg-gray-300 px-4 rounded-lg"
-		on:click={() => {
-			isOverlayOpen.set(true);
-		}}>{$semester}</button
-	>
+	<div class="relative">
+		<button
+			class="mx-4 text-lg bg-blue-600 text-white rounded-md px-2 flex focus:outline-none focus:shadow-solid"
+			on:click={() => {
+				dropdownOpen = !dropdownOpen;
+			}}
+			>{$semester}
+			<svg
+				class="w-4 h-6 ml-2"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M19 9l-7 7-7-7"
+				/></svg
+			></button
+		>
+		{#if dropdownOpen}
+			<div
+				class="absolute mx-4  bg-white text-lg z-50 list-none divide-y divide-gray-100 rounded shadow my-4"
+			>
+				<ul>
+					{#each $allSemesterNames as semesterName}
+						<li
+							class="block px-4 py-2 hover:bg-green-500 hover:text-green-100"
+							on:click={setSemester(semesterName)}
+						>
+							{semesterName}
+						</li>
+					{/each}
+				</ul>
+			</div>
+		{/if}
+	</div>
 </nav>
