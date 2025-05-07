3cdd39de9bc8deea321505c9e7015f05da3284c1
Oliver Braun <ob@cs.hm.edu>
2025-05-07 09:17:18 +0200

    feat: more info for SEB and EXaHM

--- src/routes/exam/constraints/[code=integer]/+page.server.js
+++ src/routes/exam/constraints/[code=integer]/+page.server.js
@@ -36,6 +36,9 @@ export async function load({ params }) {
                     lab
                     exahm
                     seb
+                    kdpJiraURL
+                    maxStudents
+                    comments
                 }
 			}
         }
