5cddc55905d2459ac9dd99bb0d85f660671c554f
Oliver Braun <ob@cs.hm.edu>
2025-06-05 16:26:52 +0200

    chore: rooms with houdini

--- src/routes/rooms/+page.svelte
+++ src/routes/rooms/+page.svelte
@@ -1,34 +1,38 @@
-<script>
-	export let data;
+<script lang="ts">
+	import type { PageData } from './$houdini';
+	export let data: PageData;
 
-	let rooms = data.rooms;
+	$: ({ Rooms } = data);
 
-	function onClick(column) {
+	let rooms: any = [];
+	$: rooms = $Rooms?.data?.rooms || [];
+
+	function onClick(column: any) {
 		if (column === 'nta') {
-			rooms = data.rooms.filter((r) => r.handicap);
+			rooms = $Rooms?.data?.rooms.filter((r) => r.handicap);
 		} else if (column === 'lab') {
-			rooms = data.rooms.filter((r) => r.lab);
+			rooms = $Rooms?.data?.rooms.filter((r) => r.lab);
 		} else if (column === 'placesWithSocket') {
-			rooms = data.rooms.filter((r) => r.placesWithSocket);
+			rooms = $Rooms?.data?.rooms.filter((r) => r.placesWithSocket);
 		} else if (column === 'exahm') {
-			rooms = data.rooms.filter((r) => r.exahm);
+			rooms = $Rooms?.data?.rooms.filter((r) => r.exahm);
 		} else if (column === 'seb') {
-			rooms = data.rooms.filter((r) => r.seb);
+			rooms = $Rooms?.data?.rooms.filter((r) => r.seb);
 		} else if (column === 'needsRequest') {
-			rooms = data.rooms.filter((r) => r.needsRequest);
+			rooms = $Rooms?.data?.rooms.filter((r) => r.needsRequest);
 		} else if (column === 'seats') {
-			rooms = data.rooms;
-			rooms.sort((r1, r2) => r2.seats - r1.seats);
+			rooms = $Rooms?.data?.rooms;
+			rooms?.sort((r1: any, r2: any) => r2.seats - r1.seats);
 		} else if (column === 'name') {
-			rooms = data.rooms;
-			rooms.sort((r1, r2) => r1.name.localeCompare(r2.name));
+			rooms = $Rooms?.data?.rooms;
+			rooms?.sort((r1: any, r2: any) => r1.name.localeCompare(r2.name));
 		}
 	}
 </script>
 
 <div class="text-center m-2">
 	<div class="text-4xl text-center mt-8 uppercase">
-		{data.rooms.length} Räume
+		{rooms.length} Räume
 	</div>
 </div>
 
